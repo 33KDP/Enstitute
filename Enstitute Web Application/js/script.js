630add5a05ed19ab2a7437d124773cc5292c8696
Danush99 <76690109+Danush99@users.noreply.github.com>
2021-09-21 12:05:07 +0530

    js uploaded

---
+++ Enstitute Web Application/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {             $('#loginModal').modal('show');
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+});
+
+$(document).ready(function() {             $('#sigupModal').modal('show');
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+});
