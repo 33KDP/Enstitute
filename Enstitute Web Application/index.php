f1c4cb522bd6c05a67857a365c3f54abce94d51d
supulkalhara <supul9229kalhara@gmail.com>
2021-11-09 10:44:46 +0530

    index change

--- Enstitute Web Application/index.php
+++ Enstitute Web Application/index.php
@@ -20,7 +20,7 @@
     </section>
 
         <section class="index-categories">
-            <h2>Some Basic categories</h2>
+            <h2>Somebich categories</h2>
             <div class="index-categories-list">
                 <div>
                     <h3>Timeslots</h3>   
