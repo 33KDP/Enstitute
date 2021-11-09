798c3c7af2123cabb412a8d35cae9079e929a98a
your_username <your_email_address@example.com>
2021-11-10 02:23:16 +0530

    login done

--- Enstitute Web Application/includes/login.inc.php
+++ Enstitute Web Application/includes/login.inc.php
@@ -2,17 +2,17 @@
 //we don't use ending php tag because it makes error when i type something outside of a tag
 if(isset($_POST["submit"])){
     //$email = $_POST["email"];
-    $username = $_POST["uid"];
+    $email = $_POST["uemail"];
     $pwd = $_POST["pwd"];
 
     require_once 'dbh.inc.php';
     require_once 'function.inc.php';
 
-    if(emptyInputlogin($username,$pwd) !== false){
+    if(emptyInputlogin($email,$pwd) !== false){
         header("location: ../login.php?error=emptyinput");
         exit();
     }
-    loginUser($conn,$username,$pwd);
+    loginUser($conn,$email,$pwd);
 
 }else{
     header("location: ../login.php");
