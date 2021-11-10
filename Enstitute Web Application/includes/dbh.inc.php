1620bca435704c92e318a44559b442cd2df8e44e
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2021-11-10 12:01:38 +0530

    PDO modofication

--- Enstitute Web Application/includes/dbh.inc.php
+++ Enstitute Web Application/includes/dbh.inc.php
@@ -6,7 +6,7 @@ $DBName = "Enstitute";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$DBName", $DBUsername, $DBPassword); // Others use this
-    $conn = new PDO("mysql:host=$servername;port=3307;dbname=$DBName", $DBUsername, $DBPassword); // Thivindu use this
+    // $conn = new PDO("mysql:host=$servername;port=3307;dbname=$DBName", $DBUsername, $DBPassword); // Thivindu use this
 } catch(PDOException $error){
     header("location: ../index.php?error=database_connect_error");
     $message = $error->getMessage();  
