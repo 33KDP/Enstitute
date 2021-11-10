3122b1af88149dc25b8cfcd8218801a37ff64d7c
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2021-11-10 12:01:23 +0530

    Removed gitIgnore

--- Enstitute Web Application/includes/dbh.inc.php
+++ Enstitute Web Application/includes/dbh.inc.php
@@ -5,6 +5,7 @@ $DBPassword = "33kdp";
 $DBName = "Enstitute";
 
 try {
+    $conn = new PDO("mysql:host=$servername;dbname=$DBName", $DBUsername, $DBPassword); // Others use this
     $conn = new PDO("mysql:host=$servername;port=3307;dbname=$DBName", $DBUsername, $DBPassword); // Thivindu use this
 } catch(PDOException $error){
     header("location: ../index.php?error=database_connect_error");
