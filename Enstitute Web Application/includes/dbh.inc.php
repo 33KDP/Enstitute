f2af712282b7cf658e092e168e0da14661d27277
your_username <your_email_address@example.com>
2021-11-10 11:37:06 +0530

    database error check

--- Enstitute Web Application/includes/dbh.inc.php
+++ Enstitute Web Application/includes/dbh.inc.php
@@ -7,5 +7,6 @@ $DBName = "Enstitute";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$DBName", $DBUsername, $DBPassword);
 } catch(PDOException $error){
+    header("location: ../index.php?error=database_connect_error");
     $message = $error->getMessage();  
 }
